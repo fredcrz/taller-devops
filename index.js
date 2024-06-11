11a63e0fe0bf6bfc63d7c25b41ac9a845d7d0308
Fredy Cruz <88627261+fredcrz@users.noreply.github.com>
2024-06-11 10:18:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ mongoose
   .then(() => {
     app.listen(5000, () => {
       console.log("Server has started!");
-      console.log("prueba pipeline");
+      console.log("prueba pipeline2");
     });
   });
